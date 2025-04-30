6534c88c40138d16151b6e54f2d00b6bf29c99a7
vitaFlash <vitaliaflash@gmail.com>
2025-04-30 21:17:23 +0200

    sign up

--- SignUp.java
+++ SignUp.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class SignUp {
     public static void main(String[] args) {
         //Vamos a crear un formulario de registro
@@ -7,10 +9,18 @@ public class SignUp {
         //2. Apellido
         //3. Nombre de usuario
         //4. Contraseña
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("¿Cómo es tu nombre?");
+        String name = scanner.nextLine();
+        System.out.println("¿Cómo es tu apellido?");
+        String lastName = scanner.nextLine();
+        System.out.println("¿Elige nombre de usuario?");
+        String userName = scanner.nextLine();
+        System.out.println("¿Indique tu contraseña?");
         //Añade una nueva línea antes de mostrar la siguiente pregunta.
-
+        String password = scanner.nextLine();
         //Imprimir el siguiente resultado:
+        System.out.println("\n¿Hola "+name+" "+lastName+", tu nombre de usuario es "+userName+" y tu contraseña es "+password+", gracias por registrarte.");
         //Hola <nombre> <apellido>, tu nombre de usuario es <usuario> y tu contraseña es <contraseña>, gracias por registrarte.
         //Añade una nueva línea antes de mostrar la respuesta
     }
